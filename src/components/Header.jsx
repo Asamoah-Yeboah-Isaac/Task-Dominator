2f8b9db8f8564b4fd8aea881c41b8f7c581a2ddd
Asamoah Yeboah Isaac <asamoah1357@gmail.com>
2023-09-06 08:30:03 -0700

    sub task input fields design

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,63 +1,72 @@
-import React, { useState } from 'react'
-import logo from '../assets/logo-mobile.svg'
-import downIcon from '../assets/icon-chevron-down.svg'
-import upIcon from '../assets/icon-chevron-up.svg'
-import elipsis from '../assets/icon-vertical-ellipsis.svg'
-import HeaderDropDown from './HeaderDropDown'
-import AddEditBoardModal from '../modals/AddEditBoardModal'
-import { useDispatch, useSelector } from 'react-redux'
+import React, { useState } from "react";
+import logo from "../assets/logo-mobile.svg";
+import downIcon from "../assets/icon-chevron-down.svg";
+import upIcon from "../assets/icon-chevron-up.svg";
+import elipsis from "../assets/icon-vertical-ellipsis.svg";
+import HeaderDropDown from "./HeaderDropDown";
+import AddEditBoardModal from "../modals/AddEditBoardModal";
+import { useDispatch, useSelector } from "react-redux";
+import AddEditTaskModal from "../modals/AddEditTaskModal";
 
+const Header = ({ boardModalOpen, setBoardModalOpen }) => {
+  const dispatch = useDispatch();
+  const [openDropDown, setOpenDropDown] = useState(false);
+  const [openAddEditTask, setOpenAddEditTask] = useState();
 
-const Header = ({boardModalOpen, setBoardModalOpen}) => {
-   const dispatch = useDispatch()
-  const [openDropDown, setOpenDropDown] = useState(false)
-
-//   dynamic button type 
-  const [boardType, setBoadType] = useState()
-  const boards = useSelector((state) => state.boards)
-  const board = boards.find((board) => board.isActive)
+  //   dynamic button type
+  const [boardType, setBoadType] = useState();
+  const boards = useSelector((state) => state.boards);
+  const board = boards.find((board) => board.isActive);
 
   return (
-    <div className='p-4 fixed left-0 bg-white dark:bg-[#2b2c37] z-50 right-0'>
-        
-        <header className='flex justify-between dark:text-white items-center'>
+    <div className="p-4 fixed left-0 bg-white dark:bg-[#2b2c37] z-50 right-0">
+      <header className="flex justify-between dark:text-white items-center">
         {/* left side  */}
-         <div className='flex items-center space-x-2 md:space-x-3'>
-            <img src={logo} alt="logo" className='h-6 w-6' />
-            <h3 className='hidden md:inline-block font-bold font-sans md:text-2xl'>
-                Dominator
+        <div className="flex items-center space-x-2 md:space-x-3">
+          <img src={logo} alt="logo" className="h-6 w-6" />
+          <h3 className="hidden md:inline-block font-bold font-sans md:text-2xl">
+            Dominator
+          </h3>
+          <div className="flex items-center">
+            <h3 className="truncate max-w-[200px] font-bold font-sans text-xl md:text-2xl md:ml-20   ">
+              {board.name}
             </h3>
-            <div className='flex items-center'>
-                <h3 className='truncate max-w-[200px] font-bold font-sans text-xl md:text-2xl md:ml-20   '>
-                   {board.name} 
-                </h3>
-                <img src={openDropDown ? upIcon : downIcon} alt="dropdown-icon" className='w-3 ml-2 md:hidden cursor-pointer'
-                onClick={() => setOpenDropDown(state => !state)}
-                />   
-            </div>
-         </div>
-         {/* right side  */}
-         <div className='flex space-x-4 items-center md:space-x-6'>
-             <button className='hidden md:block button '>
-                + Add New Task 
-             </button>
+            <img
+              src={openDropDown ? upIcon : downIcon}
+              alt="dropdown-icon"
+              className="w-3 ml-2 md:hidden cursor-pointer"
+              onClick={() => setOpenDropDown((state) => !state)}
+            />
+          </div>
+        </div>
+        {/* right side  */}
+        <div className="flex space-x-4 items-center md:space-x-6">
+          <button className="hidden md:block button ">+ Add New Task</button>
 
-              {/* button for mobile and small screens  */}
-             <button className='button py-1 px-3 md:hidden'>
-                +
-             </button>
-             <img src={elipsis} alt="elipsis" className='cursor-pointer h-6' />
-         </div>
-            
+          {/* button for mobile and small screens  */}
+          <button onClick={()=>{
+            setOpenAddEditTask(state => !state)
+          }} className="button py-1 px-3 md:hidden">+</button>
+          <img src={elipsis} alt="elipsis" className="cursor-pointer h-6" />
+        </div>
+      </header>
 
-        </header>
-
-        {/* drop down display for small screens */}
-         {openDropDown && <HeaderDropDown setBoardModalOpen={setBoardModalOpen} setOpenDropDown={setOpenDropDown}/>}
-         {boardModalOpen && <AddEditBoardModal type={boardType} setBoardModalOpen = {setBoardModalOpen}/>}
+      {/* drop down display for small screens */}
+      {openDropDown && (
+        <HeaderDropDown
+          setBoardModalOpen={setBoardModalOpen}
+          setOpenDropDown={setOpenDropDown}
+        />
+      )}
+      {boardModalOpen && (
+        <AddEditBoardModal
+          type={boardType}
+          setBoardModalOpen={setBoardModalOpen}
+        />
+      )}
+      {openAddEditTask && <AddEditTaskModal setOpenAddEditTask={setOpenAddEditTask}    device='mobile' />}
     </div>
+  );
+};
 
-  )
-}
-
-export default Header
+export default Header;
--- src/modals/AddEditTaskModal.jsx
+++ src/modals/AddEditTaskModal.jsx
@@ -1,34 +1,95 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
+import {v4 as uuidv4} from 'uuid'
+import crossIcon from '../assets/icon-cross.svg'
 
-const AddEditTaskModal = ({type, device, setOpenAddEditTask}) => {
-  const[title, seTitle] = useState('')
+const AddEditTaskModal = ({ type, device, setOpenAddEditTask }) => {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [subtask,setSubTask] = useState(
+    [
+      {title: '', isCompleted: false, id:uuidv4()},  
+      {title: '', isCompleted: false, id:uuidv4()},  
+    ]
+  )
+
+  const onChange = (id, newValue) => {
+    setSubTask((pervState) => {
+      const newState = [...pervState];
+      const subtask = newState.find((subtask) => subtask.id === id);
+      subtask.title = newValue;
+      return newState;
+    });
+  };
+
+  const onDelete = (id) => {
+    setSubTask((perState) => perState.filter((el) => el.id !== id))
+  }
 
   return (
-    <div onClick={(e) => {
-        if(e.target !== e.currentTarget){
-            return
+    <div
+      onClick={(e) => {
+        if (e.target !== e.currentTarget) {
+          return;
         }
-        setOpenAddEditTask(false)
-    }}
-     className={
-        device === 'mobile' ? 'py-6 px-6 pb-40 absolute overflow-y-scroll left-0 flex right-0 bottom-[-100vh] top-0 bg-[#00000080]'  : 'py-6 px-6 pb-40 absolute overflow-y-scroll left-0 flex right-0 bottom-0 top-0 bg-[#00000080]'
-    }>
-        {/* Modal section  */}
-        <div className='scrollbar-hide overflow-y-scroll max-h-[95vh] my-auto bg-white dark:[#2b2c37] text-black dark:text-white font-bold shadow-md shadow-[#364e7e1a] max-w-md mx-auto w-full px-8 py-8'>
-            <h3 className='text-lg'>
-                {type === 'edit' ? 'Edit' : 'Add New'} Task
-            </h3>
-                {/* Task Name  */}
-                <div className='mt-8 flex flex-col space-y-1'>
-                    <label className='text-sm dark:text-white text-gray-500' > 
-                        Task Name 
-                    </label>
-                    <input className='bg-transparent px-4 py-2 outline-none focus:border-0 rounded-md text-sm border border-gray-600 focus:outline-[#635fc7] ring-0' onChange={(e) =>seTitle(e.target.value)} value={title} type="text"  placeholder='e.g. Time to code'/> 
-                </div>
+        setOpenAddEditTask(false);
+      }}
+      className={
+        device === "mobile"
+          ? "py-6 px-6 pb-40 absolute overflow-y-scroll left-0 flex right-0 bottom-[-100vh] top-0 bg-[#00000080]"
+          : "py-6 px-6 pb-40 absolute overflow-y-scroll left-0 flex right-0 bottom-0 top-0 bg-[#00000080]"
+      }
+    >
+      {/* Modal section  */}
+      <div className="scrollbar-hide overflow-y-scroll max-h-[95vh] my-auto bg-white dark:[#2b2c37] text-black dark:text-white font-bold shadow-md shadow-[#364e7e1a] max-w-md mx-auto w-full px-8 py-8">
+        <h3 className="text-lg">{type === "edit" ? "Edit" : "Add New"} Task</h3>
+        {/* Task Name  */}
+        <div className="mt-8 flex flex-col space-y-1">
+          <label className="text-sm dark:text-white text-gray-500">
+            Task Name
+          </label>
+          <input
+            className="bg-transparent px-4 py-2 outline-none focus:border-0 rounded-md text-sm border border-gray-600 focus:outline-[#635fc7] ring-0"
+            onChange={(e) => setTitle(e.target.value)}
+            value={title}
+            type="text"
+            placeholder="e.g. Time to code"
+          />
         </div>
+        {/* task description  */}
+        <div className="mt-8 flex flex-col space-y-1">
+          <label className="text-sm dark:text-white text-gray-500">
+            Task Description
+          </label>
+          <textarea
+            className="bg-transparent px-4 py-2 outline-none focus:border-0 min-h-[200px] rounded-md text-sm border border-gray-600 focus:outline-[#635fc7] ring-0"
+            onChange={(e) => setDescription(e.target.value)}
+            value={description}
+            placeholder="e.g. Time for 3 hours a day coding session"
+          />
+        </div>
+        {/* subtask section */}
+        <div className="mt-8 flex flex-col space-y-1">
+          <label className="text-sm dark:text-white text-gray-500">
+            Subtasks 
+          </label>
+          {
+            subtask.map((subtask,index)=>(
+                <div onChange={(e)=>{ 
+                    onChange(subtask.id, e.target.value)
+                }}  key={index} className="flex items-center w-full">
+                  <input type="text" value={subtask.title} className="bg-transparent outline-none border focus:border-0 flex-grow px-4 py-2 rounded-md text-sm border-gray-600 focus:outline-[#635fc7]" placeholder="e.g. 3 hours non-stop coding"/>  
+                  <img  onClick={()=>{
+                     onDelete(subtask.id)
+                  }} src={crossIcon} className="m-4 cursor-pointer"/>   
+                </div>
 
+            ))
+          }
+          
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default AddEditTaskModal
+export default AddEditTaskModal;
